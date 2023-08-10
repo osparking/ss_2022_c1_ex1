f903773d8afaaf8704407e44962ad5ff2d6c2683
osparking <jbpark03@gmail.com>
2023-08-10 19:06:41 +0900

    D:/sw/Git/hello GET request handler

--- src/main/java/space/bum/controllers/DemoController.java
+++ src/main/java/space/bum/controllers/DemoController.java
@@ -1,5 +1,13 @@
 package space.bum.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "안녕?";
+	}
 }
